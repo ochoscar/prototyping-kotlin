3c5e8af50ee993e309741fb62f4e706f75318e6c
Oscar Andres Ochoa Villegas <oscar.ochoavillegas@cj.com>
2025-10-21 12:08:05 -0500

    Added currying

--- src/main/kotlin/Currying.kt
+++ src/main/kotlin/Currying.kt
@@ -2,3 +2,6 @@ package com.ochoscar
 
 fun <A, B, C> curry(f: (A, B) -> C): (A) -> (B) -> C =
     { a -> { b -> f(a, b) } }
+
+fun <A, B, C> uncurry(f: (A) -> (B) -> C): (A, B) -> C =
+    { a: A, b: B -> f(a)(b) }
